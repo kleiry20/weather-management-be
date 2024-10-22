c5ddb01800a9a87415acedec4946f78ab18a79eb
kleiry20 <74005559+kleiry20@users.noreply.github.com>
2024-10-22 20:59:53 +0530

    fixed time parameter

--- app/api/weather_fetcher.py
+++ app/api/weather_fetcher.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from datetime import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -22,14 +23,23 @@ async def fetch_weather_data():
             feels_like_kelvin = data['main']['feels_like']
             feels_like_celsius = feels_like_kelvin - 273.15
 
+            # Convert the 'dt' UNIX timestamp to a human-readable date and time
+            # Extract timestamp and convert to date and time
+            timestamp = data.get('dt')
+            date_time = datetime.fromtimestamp(timestamp)
+            date = date_time.strftime('%Y-%m-%d')
+            time = date_time.strftime('%H:%M:%S')
+
+
             # Store the data in the dictionary
             weather_data_store[city] = {
                 "temperature_celsius": round(celsius_temp, 2),
                 "feels_like_celsius": round(feels_like_celsius, 2),
                 "weather": data.get('weather', [{}])[0].get('description', 'N/A'),
                 "main": data.get('main', {}),
-                "dt": data.get('dt'),
-                "icon":data.get('weather', [{}])[0].get('icon')
+                 "date": date,
+                "time": time,
+                "icon": data.get('weather', [{}])[0].get('icon')
             }
         else:
             print(f"Error fetching data for {city}: {response.json()}")
